94f8928ed672c998353b33d69ce176b0956a50b7
GabrielGLDS <Gabeglds@hotmail.com>
2024-12-15 21:11:56 -0300

    express json o errorHandline e route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import { errorHandline } from "./middlewares/error-handling"
 
-const PORT = 3333
+import { routes } from "./routes"
+
+const PORT = 4000
 const app = express()
 app.use(express.json())
+
+app.use(routes)
+app.use(errorHandline)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))  
